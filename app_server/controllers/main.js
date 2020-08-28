c1123ea2d90d7b001984e82d8346d6e4a34c5dfe
stunning-dunning <67808790+stunning-dunning@users.noreply.github.com>
2020-08-28 17:16:29 -0500

    Bootstrap

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,5 +1,5 @@
 /* GET homepage */
-const index = (req, res) => res.render('index', { title: 'Express '});
+const index = (req, res) => res.render('index', { title: 'Datamend '});
 
 module.exports = {
   index
