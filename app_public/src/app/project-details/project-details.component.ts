1af6f58962d23fbfc85ad3c8480a398cd3e7f048
stunning-dunning <67808790+stunning-dunning@users.noreply.github.com>
2020-09-17 18:15:55 -0500

    Deploy to Heroku

--- app_public/src/app/project-details/project-details.component.ts
+++ app_public/src/app/project-details/project-details.component.ts
@@ -26,6 +26,8 @@ export class ProjectDetailsComponent implements OnInit {
   }
 
   private formIsValid(): boolean {
+    console.log("form is valid");
+    console.log(this.newBug.owner, this.newBug.status, this.newBug.description);
     if (this.newBug.owner && this.newBug.description && this.newBug.status) {
       return true;
     } else {
@@ -36,14 +38,16 @@ export class ProjectDetailsComponent implements OnInit {
   public onBugSubmit(): void {
     this.formError = '';
     if (this.formIsValid()) {
+      console.log("form");
       this.projectDataService.addBugById(this.project._id, this.newBug)
         .then((bug: Bug) => {
-          let bugs = this.project.description.slice(0);
+          let bugs = this.project.description;
           bugs.unshift(String(bug));
           this.project.description = bugs;
           this.resetAndHideBugForm();
         });
     } else {
+      console.log("eror");
       this.formError = 'All fields requried, please try again';
     } 
   }
