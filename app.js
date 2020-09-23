33e59dee4e1cbbb1172a61b4778f3eeb658cf41e
stunning-dunning <67808790+stunning-dunning@users.noreply.github.com>
2020-09-23 17:50:45 -0500

    post build

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app_public', 'build')));
+
 app.use('/api', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
